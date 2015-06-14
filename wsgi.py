704b8631cdbf45aebb2aa06d47ab04849e54a4fc
Marcello Seri <marcello.seri@gmail.com>
2015-06-14 12:33:04 +0100

    OpenShift fixes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import os
+
+IP = os.environ.get('OPENSHIFT_PYTHON_IP','127.0.0.1')
+PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT',8080))
 
 from app import app
 
@@ -7,6 +11,6 @@ from app import app
 #
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    httpd = make_server('localhost', 8051, app)
+    httpd = make_server(IP, PORT, app)
     # Wait for a single request, serve it and quit.
     httpd.serve_forever()
